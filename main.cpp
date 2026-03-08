b737a5c73ca38b194166c107e0ed7fa59870b6d3
Kris Heck <kris.heck@mailbox.org>
2026-03-07 22:30:00 -0600

    Beginnings of main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,28 @@
 #include "CPlayer.h"
 #include "CRandom.h"
 #include "CTable.h"
+#include "dominoes.h"
 
 int
 main()
 {
-    return 0;
+    CDominoes dealer;
+    CTable table;
+
+    CPlayer player1(&dealer, &table);
+    CPlayer player2(&dealer, &table);
+
+    // setup
+    dealer.create_dominoes();
+    bool player1_to_move = true; // TODO: set this variable randomly
+
+    // game loop
+    while (/* not gameover */) {
+	if (player1_to_move)
+	    player1.makeMove();
+	else
+	    player2.makeMove();
+    }
+
+    // Display results
 }
