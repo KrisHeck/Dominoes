47f013d5318e117788ff48e35d072e3fd8da1aab
Kris Heck <kris.heck@mailbox.org>
2026-03-07 22:23:31 -0600

    Starting work on CPlayer, can finish once CTable is fleshed out

---
+++ CPlayer.cpp
@@ -0,0 +1,29 @@
+#include "CPlayer.h"
+
+#include <vector>
+
+#include "CDominoes.h"
+#include "Domino.h"
+
+CPlayer::CPlayer(CDominoes *dealer, CTable *table) {
+    this->dealer = dealer;
+    this->table = table;
+}
+
+void CPlayer::giveDomino(Domino d) {
+    hand.insert(d);
+}
+
+void CPlayer::makeMove() {
+    // if (domino in hand can be played)
+    //	    place on table and remove domino from hand
+    //	    return
+    // 
+    // while (boneyard not empty)
+    //     grab domino from boneyard
+    //     if (domino can be played)
+    //         play it and remove from hand
+    //         return
+    // 
+    // if reached here, no domino can be played, turn over
+}
--- CPlayer.h
+++ CPlayer.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include <vector>
+
+#include "CDominoes.h"
+#include "Domino.h"
+
 class CPlayer {
+private:
+    CDominoes *dealer;
+    CTable *table;
+    std::vector<Domino> hand;
+
+public:
+    CPlayer(CDominoes *dealer, CTable *table);
+
+    void giveDomino(Domino d);
+    void makeMove();
 };
